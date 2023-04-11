e0f62e1e8ffad2a6f7155b8349be61b481cbfedc
Smriti <smrtghoshal24@gmail.com>
2023-04-11 22:24:24 +0530

    feat: Implement bar chart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import BarChart from "./components/BarChart";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi I am new here</div>;
+  return (
+    <div className="App">
+      <BarChart />
+    </div>
+  );
 }
 
 export default App;
