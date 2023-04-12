c0477648d6a96ec5e4bf6a3d6d28012d47614087
Smriti <smrtghoshal24@gmail.com>
2023-04-12 15:09:25 +0530

    feat: Implement DoughnutChart.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BarChart from "./components/BarChart";
+import DoughnutChart from "./components/DoughnutChart";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BarChart />
+
+      <DoughnutChart />
     </div>
   );
 }
