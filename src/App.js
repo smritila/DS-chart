0dff004b2230e92b2bd0b841ab2905970ba16f02
Smriti <smrtghoshal24@gmail.com>
2023-04-18 15:25:16 +0530

    chore: Using utility classes to build custom designs without writing CSS

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    // <DashBoard />
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
       <div>
-        {/* <DashBoard /> */}
-        <h1 className="text-6xl text-red-500 bg-purple-200">
-          Hello, Tailwind css in React!
-        </h1>
-
-        <Hello />
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
       </div>
     </div>
   );
