b50fb629f42926a6c6544123da70ace094d1b54e
Smriti <smrtghoshal24@gmail.com>
2023-04-18 18:30:53 +0530

    chore: Design table and some built in class using tailwind css.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from "react";
-import DashBoard from "./components/DashBoard";
-import Hello from "./Hello";
-
-import "./App.css";
 
 function App() {
   return (
-    <div className="container p-4">
-      {/* <DashBoard /> */}
-      <TailwindCSSButton>TailwindCSS</TailwindCSSButton>
-    </div>
-  );
-}
-
-function TailwindCSSButton(props) {
-  return (
-    <button
-      className="bg-blue-500 text-white font-medium px-4 py-2 rounded
-    hover:bg-blue-600"
-    >
-      {props.children}
-    </button>
+    <table className="table-fixed w-full">
+      <thead>
+        <tr className="bg-gray-200">
+          <th className="border border-slate-300 w-1/3 p-3 text-left ">Song</th>
+          <th className="border border-slate-300 w-1/3 p-3 text-left">
+            Artist
+          </th>
+          <th className="border border-slate-300 w-1/3 p-3 text-left">Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="bg-gray-600 border border-gray-400">
+          <td className="border border-slate-300 w-1/3 p-3 text-blue-400">
+            The Sliding Mr. Bones (Next Stop, Pottersville)
+          </td>
+          <td className="border border-slate-300 w-1/3 p-3  text-blue-400">
+            Malcolm Lockyer
+          </td>
+          <td className="border border-slate-300 w-1/3 p-3 text-blue-400">
+            1961
+          </td>
+        </tr>
+        <tr className="bg-gray-100 border border-gray-400">
+          <td className="border border-slate-300w-1/3 p-3">Witchy Woman</td>
+          <td className="border border-slate-300w-1/3 p-3">The Eagles</td>
+          <td className="border border-slate-300w-1/3 p-3">1972</td>
+        </tr>
+        <tr className="bg-white border border-gray-400">
+          <td className="border border-slate-300w-1/3 p-3">Shining Star</td>
+          <td className="border border-slate-300w-1/3 p-3">
+            Earth, Wind, and Fire
+          </td>
+          <td className="border border-slate-300w-1/3 p-3">1975</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
--- src/Hello.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Hello() {
-  return <div className="text-2xl">Hello</div>;
-}
-
-export default Hello;
