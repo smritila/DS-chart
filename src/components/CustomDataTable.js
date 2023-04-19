c65de73631b5da850c71c81a0669269b3e30dc90
Smriti <smrtghoshal24@gmail.com>
2023-04-19 15:05:13 +0530

    feat: implement sorting in react data table.

--- src/components/CustomDataTable.js
+++ src/components/CustomDataTable.js
@@ -4,15 +4,18 @@ const columns = [
   {
     name: "Title",
     selector: (row) => row.title,
+    sortable: true,
   },
 
   {
     name: "Director",
     selector: (row) => row.director,
+    sortable: true,
   },
   {
     name: "Year",
     selector: (row) => row.year,
+    sortable: true,
   },
 ];
 
