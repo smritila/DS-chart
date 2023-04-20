1e405bcdaacc87e7d8b39a8d5f851328fd4d9994
Smriti <smrtghoshal24@gmail.com>
2023-04-20 17:49:27 +0530

    chore: implement side bar component using flex.

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="h-screen bg-gray-100 flex flex-col justify-center items-center w-1/4">
+      <button className="bg-purple-500 text-white py-2 px-4 rounded-full font-bold">
+        Business Specific
+      </button>
+    </div>
+  );
+}
+
+export default Sidebar;
