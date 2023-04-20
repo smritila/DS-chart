0a3995502bbc71b55d6bd335f6229bbde89355b5
Smriti <smrtghoshal24@gmail.com>
2023-04-20 21:06:43 +0530

    chore: implement button design in side bar component.

--- src/components/CustomDataTable.js
+++ src/components/CustomDataTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import DataTable from "react-data-table-component";
 import "tailwindcss/tailwind.css";
 
@@ -112,59 +112,12 @@ const data = [
 ];
 
 function CustomDataTable() {
-  const [filteredData, setFilteredData] = useState(data);
-  const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(10);
-
-  const handleFilter = (event) => {
-    const value = event.target.value.toLowerCase();
-    const filtered = data.filter((item) => {
-      return (
-        item.title.toLowerCase().includes(value) ||
-        item.director.toLowerCase().includes(value) ||
-        item.year.toLowerCase().includes(value)
-      );
-    });
-    setFilteredData(filtered);
-    setPage(1);
-  };
-
-  const handlePageChange = (page) => {
-    setPage(page);
-  };
-
-  const handlePerPageChange = (perPage) => {
-    setPerPage(perPage);
-  };
-
-  const paginatedData = filteredData.slice(
-    (page - 1) * perPage,
-    page * perPage
-  );
-
   return (
-    <div className="p-4 font-sans">
-      <h1 className="text-md font-sans mb-4">Users</h1>
-
-      <input
-        type="text"
-        placeholder="Filter..."
-        onChange={handleFilter}
-        className="px-4 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-      />
-      <DataTable
-        columns={columns}
-        data={paginatedData}
-        pagination
-        paginationServer
-        paginationTotalRows={filteredData.length}
-        paginationPerPage={perPage}
-        paginationRowsPerPageOptions={[10, 25, 50, 100]}
-        onChangeRowsPerPage={handlePerPageChange}
-        onChangePage={handlePageChange}
-        className="mt-4"
-      />
-    </div>
+    <DataTable
+      className="rounded-lg w-full p-4"
+      columns={columns}
+      data={data}
+    ></DataTable>
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <div className="h-screen bg-white flex flex-col justify-start items-center w-1/8 p-4">
+    <div className="h-screen bg-white flex flex-col justify-start items-center w-1/6 p-4">
       <h1 className="text-2xl font-bold text-black mb-4">Vidhi</h1>
-      <button className="bg-blue-100 text-blue-700 py-2 px-2 rounded-md  top-4 left-4">
+      <button className="bg-blue-100 text-blue-700 px-2 py-2 text-sm w-25 rounded">
         Business Specific
       </button>
     </div>
