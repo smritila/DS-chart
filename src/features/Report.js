3bfecc1527ae233704e19149283dacf682601207
Smriti <smrtghoshal24@gmail.com>
2023-04-20 17:50:18 +0530

    chore: implement sidebar component using flex

--- src/features/Report.js
+++ src/features/Report.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import CustomDataTable from "../components/CustomDataTable";
+import Sidebar from "../components/Sidebar";
 
 function Report() {
   return (
-    <div>
+    <div className="flex flex-row">
+      <Sidebar />
       <CustomDataTable />
     </div>
   );
