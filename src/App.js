75e1e4b3e6ec41f41637143f1bd365cff85cda58
Smriti <smrtghoshal24@gmail.com>
2023-04-25 12:33:21 +0530

    chore: Implement two different routes for DashBoard and Report page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
-
 import "./App.css";
-
-import DashBoard from "./features/DashBoard";
 import Report from "./features/Report";
 import Sidebar from "./components/Sidebar";
+import DashBoard from "./features/DashBoard";
 
 function App() {
   return (
-    <div className="bg-gray-100 flex flex-row">
-      <Sidebar />
-      <main className="flex-1 p-4 overflow-y-auto ml-sidebar">
-        <h1 className="text-xl font-bold">Loreum Ipsum</h1>
-        <hr className="border-t-1 my-5 border-gray-300" />
-        <Report />
-      </main>
-    </div>
+    <Router>
+      <div className="bg-gray-100 flex flex-row">
+        <Sidebar />
+        <main className="flex-1 p-4 overflow-y-auto ml-sidebar">
+          <h1 className="text-xl font-bold">Loreum Ipsum</h1>
+          <hr className="border-t-1 my-5 border-gray-300" />
+          <Routes>
+            <Route path="/Dashboard" element={<DashBoard />} />
+            <Route path="/Report" element={<Report />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
